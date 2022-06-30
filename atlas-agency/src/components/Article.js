81cb498199845810a330d58f1178f5cd1eb19654
Ava <avamsteimle@gmail.com>
2022-06-30 10:53:18 -0700

    main content started and styled

--- atlas-agency/src/components/Article.js
+++ atlas-agency/src/components/Article.js
@@ -1,7 +1,7 @@
 const Article = () => {
     return(
         <div style={{paddingTop: "40px"}}>
-            <h1>Atlas Agency</h1>
+            <h1 style={{fontSize: "32px", fontWeight: "700", letterSpacing: ".5px"}}>Atlas Agency</h1>
             <p>Looking for a leading ad agency in Los Angeles?</p>
         </div>
     );
