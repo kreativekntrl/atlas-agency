6cae9005ebe1576cea4fdc228cdbb845a3ba49d1
Ava <avamsteimle@gmail.com>
2022-06-30 17:24:23 -0700

    footer

--- atlas-agency/src/components/Footer.js
+++ atlas-agency/src/components/Footer.js
@@ -1,7 +1,11 @@
+import {SocialMediaIconsReact} from 'social-media-icons-react';
 const Footer = () => {
     return(
         <div>
-
+            <p>lets get in contact:</p>
+            <SocialMediaIconsReact className="social-icon" borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(117,123,127,0.89)" iconSize="5" roundness="50%" url="https://some-website.com/my-social-media-url" size="30" />
+            <SocialMediaIconsReact className="social-icon" borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(117,123,127,0.89)" iconSize="5" roundness="50%" url="https://some-website.com/my-social-media-url" size="30" />
+            <SocialMediaIconsReact className="social-icon" borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="mail" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(117,123,127,0.89)" iconSize="5" roundness="50%" url="https://some-website.com/my-social-media-url" size="30" />
         </div>
     );
 }
