60a433963139eeb4a9b99471f8e5f38321982eaa
Ava <avamsteimle@gmail.com>
2022-06-30 18:24:43 -0700

    article styles

--- atlas-agency/src/index.js
+++ atlas-agency/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
